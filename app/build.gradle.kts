28a19a9ef8b7b24c6538fe23a549fe37c3be287c
Claire Nicole <cnbpets@gmail.com>
2026-06-07 20:03:35 -0600

    Added profile image picker using Android file picker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 }
 
 dependencies {
+    implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
--- app/src/main/java/com/bignerdranch/android/healthyhabittracker/screens/ProfileScreen.kt
+++ app/src/main/java/com/bignerdranch/android/healthyhabittracker/screens/ProfileScreen.kt
@@ -6,9 +6,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.draw.clip
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import androidx.compose.runtime.*
 
 @Composable
 fun ProfileScreen() {
+
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val launcher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.GetContent()
+        ) { uri ->
+
+            imageUri = uri
+        }
 
     Column(
         modifier = Modifier
@@ -18,7 +41,27 @@ fun ProfileScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        if (imageUri != null) {
 
+            Image(
+                painter = rememberAsyncImagePainter(imageUri),
+                contentDescription = "Profile Picture",
+
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape),
+
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Button(
+            onClick = {
+                launcher.launch("image/*")
+            }
+        ) {
+            Text("Choose Profile Picture")
+        }
         Text(
             text = "Profile",
             style = MaterialTheme.typography.headlineMedium
@@ -44,14 +87,14 @@ fun ProfileScreen() {
                 Text("Habits Tracked: 4")
                 Text("Current Streak: 5 Days")
                 Text("Weekly Goal: Stay Consistent")
-
-                Text(
-                    text = "Database Status: Room database integration in progress."
-                )
             }
         }
 
         Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            text = "Database Status: Room database integration in progress."
+        )
 
         Text(
             text = "Built using Kotlin and Jetpack Compose",
