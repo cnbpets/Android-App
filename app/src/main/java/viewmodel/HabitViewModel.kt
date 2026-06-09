d6e171a265dc7974ede8c5079aa7d6ba0b55dcf1
Claire Nicole <cnbpets@gmail.com>
2026-06-09 17:49:50 -0600

    Added insert and delete functionality to HabitViewModel

--- app/src/main/java/viewmodel/HabitViewModel.kt
+++ app/src/main/java/viewmodel/HabitViewModel.kt
@@ -1,8 +1,35 @@
 package com.bignerdranch.android.healthyhabittracker.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bignerdranch.android.healthyhabittracker.data.Habit
 import com.bignerdranch.android.healthyhabittracker.data.HabitRepository
+import kotlinx.coroutines.launch
 
 class HabitViewModel(
     private val repository: HabitRepository
-) : ViewModel()
+) : ViewModel() {
+
+    fun insertHabit(
+        title: String,
+        frequency: String
+    ) {
+
+        viewModelScope.launch {
+
+            repository.insertHabit(
+                Habit(
+                    title = title,
+                    frequency = frequency
+                )
+            )
+        }
+    }
+
+    fun deleteHabit(habit: Habit) {
+
+        viewModelScope.launch {
+            repository.deleteHabit(habit)
+        }
+    }
+}
